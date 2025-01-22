6a056bf4c5302db6d5fd996cec6570984d9eb04c
Ritikjain91 <jainritik829@gmail.com>
2025-01-22 07:43:22 +0530

    update the code

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import './App.css';
 
 
-const ENDPOINT = "http://localhost:3000"; // Backend server URL
+const ENDPOINT = "https://chat-backend-app-i725.vercel.app/"; // Backend server URL
 
 function App() {
   const [messages, setMessages] = useState([]);
